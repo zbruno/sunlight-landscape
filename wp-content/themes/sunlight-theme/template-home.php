7c0a9d1d855cc26d0bf8422be6dcda7b24d59b49
Zachary Bruno <zachcbruno@gmail.com>
2016-03-06 16:28:41 -0800

    update theme

--- wp-content/themes/sunlight-theme/template-home.php
+++ wp-content/themes/sunlight-theme/template-home.php
@@ -10,3 +10,33 @@
   <h2 class="sl-home-hero-subtitle">The trusted source in quality full-service landscaping.</h2>
   <a href="" class="sl-home-hero-button">Get started Now</a>
 </div>
+<div class="sl-home-certifications">
+  <h2 class="sl-home-certifications-title">Certifications</h2>
+  <h3 class="sl-home-certifications-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, voluptate, accusamus sint dignissimos</h3>
+  <div class="sl-home-certifications-container">
+    <div class="sl-home-certification">
+      <img src="/wp-content/themes/sunlight-theme/dist/images/lic.jpg" alt="" class="sl-home-certification-image">
+      <p class="sl-home-certification-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa commodi illo perferendis reprehenderit praesentium quisquam accusantium, nam provident quo ratione laboriosam, quibusdam sunt atque non, quis itaque saepe suscipit sed.</p>
+      <a class="sl-home-certification-link" href="https://www.landscapeprofessionals.org/" target="_blank">
+        <i class="material-icons icon">search</i>
+        <span class="text">Learn More</span>
+      </a>
+    </div>
+    <div class="sl-home-certification">
+      <img src="/wp-content/themes/sunlight-theme/dist/images/bbb.jpg" alt="" class="sl-home-certification-image">
+      <p class="sl-home-certification-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa commodi illo perferendis reprehenderit praesentium quisquam accusantium, nam provident quo ratione laboriosam, quibusdam sunt atque non, quis itaque saepe suscipit sed.</p>
+      <a class="sl-home-certification-link" href="http://www.bbb.org/sdoc/business-reviews/landscape-contractors/sunlite-landscape-inc-in-el-cajon-ca-10015372" target="_blank">
+        <i class="material-icons icon">search</i>
+        <span class="text">Learn More</span>
+      </a>
+    </div>
+    <div class="sl-home-certification">
+      <img src="/wp-content/themes/sunlight-theme/dist/images/clca.jpg" alt="" class="sl-home-certification-image">
+      <p class="sl-home-certification-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa commodi illo perferendis reprehenderit praesentium quisquam accusantium, nam provident quo ratione laboriosam, quibusdam sunt atque non, quis itaque saepe suscipit sed.</p>
+      <a class="sl-home-certification-link" href="www.clca.org" target="_blank">
+        <i class="material-icons icon">search</i>
+        <span class="text">Learn More</span>
+      </a>
+    </div>
+  </div>
+</div>
