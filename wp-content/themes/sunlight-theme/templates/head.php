809b5e221317aa614d56634b580bfcef87c820f5
Zachary Bruno <zachcbruno@gmail.com>
2016-03-06 16:25:57 -0800

    add material icons

--- wp-content/themes/sunlight-theme/templates/head.php
+++ wp-content/themes/sunlight-theme/templates/head.php
@@ -2,6 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700|Roboto:400,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Material+Icons|Raleway:400,500,300,700' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
 </head>
